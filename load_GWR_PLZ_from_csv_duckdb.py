62cce87639de48a457f6e4be1e19e9f34e8bbef4
Lorenz Schiffmann <lorenz.schiffmann@gmail.com>
2023-07-01 16:30:00 +0200

    Initial version

---
+++ load_GWR_PLZ_from_csv_duckdb.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 15 16:39:31 2022
+
+@author: u237679
+"""
+
+import urllib.request
+import duckdb
+from pathlib import Path
+import shutil
+import time
+import zipfile
+
+DOWNLOAD_BFS_EGID = True
+DOWNLOAD_POST_PLZ_POP = True
+LOAD_EINGANG = True
+LOAD_GEBAEUDE = True
+LOAD_WOHNUNG = True
+LOAD_CODES = True
+LOAD_PLZ_POP = True
+
+bfs_data_link="https://public.madd.bfs.admin.ch/ch.zip" # all available BFS data for CH
+post_plz_pop_data_link = 'https://swisspost.opendatasoft.com/api/explore/v2.1/catalog/datasets/bevoelkerung_proplz/exports/csv?lang=de&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B'
+downloads_path = '~/Downloads'
+# Dynamic path resolving, e.g. C:\Users\<user>\Downloads on Windows
+# or /home/<user>/Downloads on Linux
+downloads_path_str = str(Path(downloads_path).expanduser())
+
+documents_path = '~/Downloads'
+documents_path_str = str(Path(documents_path).expanduser())
+
+base_path_str  = downloads_path_str
+
+gwr_eingang_filepath =  base_path_str + '/ch/eingang_entree_entrata.csv'
+gwr_eingang_table = "ENTRANCE" 
+
+gwr_gebaeude_filepath = base_path_str + '/ch/gebaeude_batiment_edificio.csv'
+gwr_gebaeude_table = "BUILDING" 
+
+gwr_wohnung_filepath = base_path_str + '/ch/wohnung_logement_abitazione.csv'
+gwr_wohnung_table = "DWELLING" 
+
+gwr_codes_filepath =  base_path_str + '/ch/kodes_codes_codici.csv'
+gwr_codes_table = "CODES" 
+
+plz_pop_filepath = base_path_str +"/bevoelkerung_proplz.csv"
+plz_pop_table = "PLZ_POP" 
+
+
+DUCK_DB = 'gwr_ch_bfs_duck.db'
+PATH_TO_DUCK_DB = documents_path_str + "/" + DUCK_DB
+
+
+gwr_building_community_v = """
+CREATE OR REPLACE VIEW GWR_BUILDING_COMMUNITY_V AS
+SELECT b.EGID, e.EDID ,  b.GDEKT, e.DPLZ4, e.DPLZNAME  FROM BUILDING b
+JOIN  ENTRANCE e ON b.EGID = e.EGID;
+""" 
+
+gwr_dwelling_community_v = """
+CREATE OR REPLACE VIEW GWR_DWELLING_COMMUNITY_V AS
+SELECT b.EGID, e.EDID, d.EWID,  b.GDEKT, e.DPLZ4, e.DPLZNAME  FROM BUILDING b
+JOIN  ENTRANCE e ON b.EGID = e.EGID
+JOIN  DWELLING d ON b.EGID = d.EGID; 
+"""
+
+
+load_mapping = [[gwr_eingang_filepath,gwr_eingang_table],
+                [gwr_gebaeude_filepath,gwr_gebaeude_table],
+                [gwr_wohnung_filepath,gwr_wohnung_table],
+                [gwr_codes_filepath,gwr_codes_table],
+                [plz_pop_filepath, plz_pop_table]
+                ]
+
+def download_file(url, file = None, proxies = None, verify = None):
+    time_start = time.time() 
+    #create the object, assign it to a variable
+    proxy = urllib.request.ProxyHandler(proxies)
+    # construct a new opener using your proxy settings
+    opener = urllib.request.build_opener(proxy)
+    # install the openen on the module-level
+    urllib.request.install_opener(opener)    
+    file_path, _ = urllib.request.urlretrieve(url)
+    if file:
+        shutil.copy(file_path, file)
+    print(file_path)
+    time_end=time.time()
+    print('=> Runtime of %s: %.2f second.' %("Download/Extract " + url, time_end-time_start)) 
+    return file_path
+
+
+def download_extract_zip(url, extract_dir, proxies = None, verify = None):
+    time_start = time.time() 
+    zip_path = download_file(url, file = None, proxies = proxies, verify = verify)
+    with zipfile.ZipFile(zip_path, "r") as f:
+        f.extractall(extract_dir + "/ch")
+    time_end=time.time()
+    print('=> Runtime of %s: %.2f second.' %("Download/Extract " + url, time_end-time_start)) 
+    
+ 
+      
+def load_save_csv(engine, load_pair):
+    time_start = time.time()    
+    engine.execute("DROP TABLE IF EXISTS " + load_pair[1])    
+    engine.from_csv_auto(load_pair[0]).create(load_pair[1])    
+    df = engine.execute("SELECT COUNT(*) FROM " + load_pair[1]).df() 
+    nbr_rows = df['count_star()'].iat[0]
+    time_end=time.time() 
+    print('=> Runtime of %s: %.2f second.' %("Dumping " + load_pair[0] + " to Table " + load_pair[1] 
+                                             + " (" + str(nbr_rows) + " rows)", time_end-time_start))               
+       
+if __name__ == "__main__":
+    proxies, verify =  {}, True # to be adapted when running on a corporate device/network
+    download_extract_zip(bfs_data_link, extract_dir = downloads_path_str,
+                        proxies = proxies, verify = verify) if DOWNLOAD_BFS_EGID else None    
+    download_file(post_plz_pop_data_link, file=plz_pop_filepath, proxies = proxies, verify = None) if DOWNLOAD_POST_PLZ_POP else None    
+    engine = duckdb.connect(database = PATH_TO_DUCK_DB)    
+    load_save_csv(engine, load_mapping[0]) if LOAD_EINGANG else None
+    load_save_csv(engine, load_mapping[1]) if LOAD_GEBAEUDE else None
+    load_save_csv(engine, load_mapping[2]) if LOAD_WOHNUNG else None
+    load_save_csv(engine, load_mapping[3]) if LOAD_CODES else None
+    load_save_csv(engine, load_mapping[4]) if LOAD_PLZ_POP else None
+    #df = engine.execute(gwr_building_community_v)
+    #df = engine.execute(gwr_dwelling_community_v)
+    engine.close()
+
+
+
+
---
+++ openai_sql_duckdb.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 22 01:05:46 2023
+
+@author: schiffma
+"""
+
+import openai
+import os
+import re
+import duckdb
+import sys
+from tabulate import tabulate
+import time
+from dotenv import load_dotenv
+
+
+load_dotenv()
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+#db = chinook.db"
+context = "gwr_ch_bfs"
+db = context + "_duck.db"
+db_file = "data/" + db
+context_file = "data/" + context + "_context.txt"
+select_pattern = '(SELECT|WITH).+?([;]|[`]{3})' # find SQL non-greedy !!!
+
+
+
+def read_txt_file(file_name):
+    with open(file_name, mode="r", encoding="utf-8") as file:
+        text_str = file.read()
+    return text_str
+
+def open_db():
+    conn = duckdb.connect(db_file, read_only=True) # just queries
+    return conn
+
+def eval_sql(conn, sql, answer):
+    eval_sql_res = False
+    time_start = time.time()
+    try:
+        df = conn.sql(sql).df()
+        result = tabulate(df, headers='keys', tablefmt='psql') + "\n" + sql
+        eval_sql_res = True
+    except Exception as e:
+        result = f"SQL-Error {e} for:\n{answer}"
+        eval_sql_res = False
+    time_end=time.time() 
+    print('\n>> Total Runtime of %s: %.2f second.' 
+          %(sys._getframe().f_code.co_name,
+          time_end-time_start))        
+    return result, eval_sql_res
+
+def open_ai_sql(messages):
+    time_start = time.time() 
+    time_start_openai = time.time()
+    chat = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        temperature = 0.1,
+        n = 1,
+        max_tokens=2048,
+        messages=messages
+        
+    )
+    time_end_openai=time.time()
+    print("prompt_tokens: ", chat.usage["prompt_tokens"])
+    total_tokens = int(chat.usage["total_tokens"])
+    print("total_tokens: ", total_tokens)
+    print('>> Runtime of OpenAI: %.2f second.' 
+          %(time_end_openai-time_start_openai))
+    reply = chat.choices[0].message.content            
+    eval_sql_res = False
+
+    answer_1line = " ".join(reply.splitlines())            
+    sql = ""
+    matches = re.finditer(select_pattern, answer_1line)
+    for match in matches: # get last sql
+        sql = match.group()
+    if len(sql) > 0:    
+        # print(f"SQL found in answer: {sql}")
+        sql = sql.replace('`','')
+        reply, eval_sql_res = eval_sql(conn, sql, reply)
+    else:
+        reply  = f"SQL not found in reply:\n{reply}"
+    nl = "\n" if eval_sql_res else ""
+    print(f"\nChatGPT[{db}]: {nl}{reply}")
+    time_end=time.time() 
+    print('\n> Total Runtime of %s: %.2f second.' 
+          %(sys._getframe().f_code.co_name,
+          time_end-time_start))        
+
+def sql_answer(conn, context_text):
+    messages = []
+    messages.append({"role": "system", "content": context_text})
+    while True:        
+        query = input("\nQuestion: ")
+        #query = prompt("\nQuestion: ")
+        if query == "exit":
+            break    
+        if query:           
+            # query = context_text + query
+            messages.append({"role": "user", "content": query})                        
+            try:  
+                open_ai_sql(messages)
+                context_text = ""         
+            except Exception as e:
+              print(f"OpenAI-Error {e}")
+
+
+if __name__ == "__main__":
+    context_text = read_txt_file(context_file)
+    conn = open_db() 
+    sql_answer(conn, context_text)
+    conn.close()
