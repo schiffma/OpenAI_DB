83fcb2e27370987ccd3341ff7f8294dcc1bcad85
schiffma <lorenz.schiffmann@gmail.com>
2023-08-12 09:49:34 +0200

    Update openai_TK.py

--- openai_TK.py
+++ openai_TK.py
@@ -5,14 +5,6 @@ Created on Sun Jul 30 17:51:28 2023
 @author: schiffma
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul 30 16:23:36 2023
-
-@author: schiffma
-"""
-
-
 openai_tk = """
 ________                              _____   .___      ___________ ____  __. 
 \_____  \  ______    ____    ____    /  _  \  |   |     \__    ___/|    |/ _| 
@@ -49,17 +41,21 @@ class ChatInterface(tk.Frame):
         self.history = []
         self.history_index = -1
 
+
+        colors =  { "Default": {'bg': "#FFFFFF", "bg2": "#EEEEEE", "fg" : "#000000" },
+                  "Dark": {'bg': "#212121", "bg2": "#2a2b2d", "fg" : "#FFFFFF" },
+                  "Grey": {'bg':  "#4f4f4f", "bg2": "#444444", "fg" : "#ffffff" },
+                  "Torque": {'bg': "#669999", "bg2":  "#003333", "fg" : "#FFFFFF" },
+                  "Darkblue": {'bg': "#1c2e44", "bg2":  "#263b54", "fg" : "#FFFFFF" },
+                  "Hacker": {'bg': "#0F0F0F", "bg2":  "#0F0F0F", "fg" : "#33FF33"},
+                  "Hacker2": {'bg': "#0F0F0F", "bg2":  "#0F0F0F", "fg" : "#FF8C00"}}
+
         # sets default bg for top level windows
         
         
-        # self.tl_bg = "#EEEEEE"
-        # self.tl_bg2 = "#EEEEEE"
-        # self.tl_fg = "#000000"
-        # self.font = "Verdana 10"
-        
-        self.tl_bg = "#0F0F0F"
-        self.tl_bg2 = "#0F0F0F"
-        self.tl_fg = "#33FF33"
+        self.tl_bg = colors["Hacker"]["bg"]
+        self.tl_bg2 = colors["Hacker"]["bg2"]
+        self.tl_fg = colors["Hacker"]["fg"]
         self.font = "fixedsys"        
 
         menu = tk.Menu(self.master)
@@ -81,39 +77,15 @@ class ChatInterface(tk.Frame):
         contexts.add_command(label="SQL/Global Power Plants",command=self.gpp)
         contexts.add_command(label="SQL/GWR CH",command=self.gwr_ch_bfs)
 
-    # Options
-        options = tk.Menu(menu, tearoff=0)
-        menu.add_cascade(label="Options", menu=options)        
-
-        # font
-        font =tk.Menu(options, tearoff=0)
-        options.add_cascade(label="Font", menu=font)
-        font.add_command(label="Default",command=self.font_change_default)
-        font.add_command(label="Times",command=self.font_change_times)
-        font.add_command(label="System",command=self.font_change_system)
-        font.add_command(label="Helvetica",command=self.font_change_helvetica)
-        font.add_command(label="Fixedsys",command=self.font_change_fixedsys)
-
-        # color theme
-        color_theme = tk.Menu(options, tearoff=0)
-        options.add_cascade(label="Color Theme", menu=color_theme)
-        color_theme.add_command(label="Default",command=self.color_theme_default) 
-       # color_theme.add_command(label="Night",command=self.) 
-        color_theme.add_command(label="Grey",command=self.color_theme_grey) 
-        color_theme.add_command(label="Blue",command=self.color_theme_dark_blue) 
        
-        color_theme.add_command(label="Torque",command=self.color_theme_turquoise)
-        color_theme.add_command(label="Hacker",command=self.color_theme_hacker)
-       # color_theme.add_command(label='Mkbhd',command=self.MKBHD)
-
-
-      
         help_option = tk.Menu(menu, tearoff=0)
         menu.add_cascade(label="Help", menu=help_option)
         #help_option.add_command(label="Features", command=self.features_msg)
         help_option.add_command(label="About PyBot", command=self.msg)
-        help_option.add_command(label="Develpoers", command=self.about)
-
+        help_option.add_command(label="Developers", command=self.about)
+        
+ 
+     
         self.text_frame = tk.Frame(self.master, bd=6)
         self.text_frame.pack(expand=True, fill=tk.BOTH)
 
@@ -152,11 +124,35 @@ class ChatInterface(tk.Frame):
         self.master.bind("<Up>",self.cycleHistory)
         
         self.last_sent_label(date="No messages sent.")
-        
-        self.font_change_fixedsys()
-        self.color_theme_hacker()
+
+         # Options
+        options = tk.Menu(menu, tearoff=0)
+        menu.add_cascade(label="Options", menu=options)         
+        # font
+        font =tk.Menu(options, tearoff=0)
+        options.add_cascade(label="Font", menu=font)
+        font.add_command(label="Default",command=lambda: self.font_change_generic("Verdana 10"))
+        font.add_command(label="Times",command=lambda: self.font_change_generic("Times"))
+        font.add_command(label="System",command=lambda: self.font_change_generic("System"))
+        font.add_command(label="Helvetica",command=lambda: self.font_change_generic("helvetica 10"))
+        font.add_command(label="Fixedsys",command= lambda: self.font_change_generic("fixedsys")) 
+           
+     
+         # color theme
+        color_theme = tk.Menu(options, tearoff=0)
+        options.add_cascade(label="Color Theme", menu=color_theme)
+        color_theme.add_command(label="Default",command=lambda: self.color_theme_generic(colors["Default"])) 
+        color_theme.add_command(label="Dark",command=lambda: self.color_theme_generic(colors["Dark"])) 
+        color_theme.add_command(label="Grey",command=lambda: self.color_theme_generic(colors["Grey"])) 
+        color_theme.add_command(label="Blue",command=lambda: self.color_theme_generic(colors["Darkblue"]))        
+        color_theme.add_command(label="Torque",command=lambda: self.color_theme_generic(colors["Torque"]))
+        color_theme.add_command(label="Hacker",command=lambda: self.color_theme_generic(colors["Hacker"]))
+        color_theme.add_command(label="Hacker2",command=lambda: self.color_theme_generic(colors["Hacker2"]))
+            
+        self.font_change_generic("fixedsys")
+        self.color_theme_generic(colors["Hacker"]) 
+    
         self.__append_to_text_box(openai_tk)
-        
         self.openaibot = ChatBotOpenAI()
         
 
@@ -189,7 +185,7 @@ class ChatInterface(tk.Frame):
     def general(self):
         self.bot_prompt = self.base_prompt
         context = "general"
-        self.openaibot.set_context_text(context)
+        self.openaibot.set_context_str(context)
         self.__append_to_text_box(f"SQL-Context: {context} loaded.\n\n")
         
         
@@ -240,138 +236,25 @@ class ChatInterface(tk.Frame):
             self.entry_field.insert(0,comm)   
 
 
-
-    def font_change_default(self):
-        self.text_box.config(font="Verdana 10")
-        self.entry_field.config(font="Verdana 10")
-        self.font = "Verdana 10"
-
-    def font_change_times(self):
-        self.text_box.config(font="Times")
-        self.entry_field.config(font="Times")
-        self.font = "Times"
-
-    def font_change_system(self):
-        self.text_box.config(font="System")
-        self.entry_field.config(font="System")
-        self.font = "System"
-
-    def font_change_helvetica(self):
-        self.text_box.config(font="helvetica 10")
-        self.entry_field.config(font="helvetica 10")
-        self.font = "helvetica 10"
-
-    def font_change_fixedsys(self):
-        self.text_box.config(font="fixedsys")
-        self.entry_field.config(font="fixedsys")
-        self.font = "fixedsys"
+    def font_change_generic(self, font):
+        self.text_box.config(font=font)
+        self.entry_field.config(font=font)
+        self.font = font
 
 
-
-    def color_theme_default(self):
-        self.master.config(bg="#EEEEEE")
-        self.text_frame.config(bg="#EEEEEE")
-        self.entry_frame.config(bg="#EEEEEE")
-        self.text_box.config(bg="#FFFFFF", fg="#000000")
-        self.entry_field.config(bg="#FFFFFF", fg="#000000", insertbackground="#000000")
-        self.send_button_frame.config(bg="#EEEEEE")
-        self.send_button.config(bg="#FFFFFF", fg="#000000", activebackground="#FFFFFF", activeforeground="#000000")
-        #self.emoji_button.config(bg="#FFFFFF", fg="#000000", activebackground="#FFFFFF", activeforeground="#000000")
-        self.sent_label.config(bg="#EEEEEE", fg="#000000")
-
-        self.tl_bg = "#FFFFFF"
-        self.tl_bg2 = "#EEEEEE"
-        self.tl_fg = "#000000"
-
-    # Dark
-    def color_theme_dark(self):
-        self.master.config(bg="#2a2b2d")
-        self.text_frame.config(bg="#2a2b2d")
-        self.text_box.config(bg="#212121", fg="#FFFFFF")
-        self.entry_frame.config(bg="#2a2b2d")
-        self.entry_field.config(bg="#212121", fg="#FFFFFF", insertbackground="#FFFFFF")
-        self.send_button_frame.config(bg="#2a2b2d")
-        self.send_button.config(bg="#212121", fg="#FFFFFF", activebackground="#212121", activeforeground="#FFFFFF")
-       # self.emoji_button.config(bg="#212121", fg="#FFFFFF", activebackground="#212121", activeforeground="#FFFFFF")
-        self.sent_label.config(bg="#2a2b2d", fg="#FFFFFF")
-
-        self.tl_bg = "#212121"
-        self.tl_bg2 = "#2a2b2d"
-        self.tl_fg = "#FFFFFF"
-
-    # Grey
-    def color_theme_grey(self):
-        self.master.config(bg="#444444")
-        self.text_frame.config(bg="#444444")
-        self.text_box.config(bg="#4f4f4f", fg="#ffffff")
-        self.entry_frame.config(bg="#444444")
-        self.entry_field.config(bg="#4f4f4f", fg="#ffffff", insertbackground="#ffffff")
-        self.send_button_frame.config(bg="#444444")
-        self.send_button.config(bg="#4f4f4f", fg="#ffffff", activebackground="#4f4f4f", activeforeground="#ffffff")
-        #self.emoji_button.config(bg="#4f4f4f", fg="#ffffff", activebackground="#4f4f4f", activeforeground="#ffffff")
-        self.sent_label.config(bg="#444444", fg="#ffffff")
-
-        self.tl_bg = "#4f4f4f"
-        self.tl_bg2 = "#444444"
-        self.tl_fg = "#ffffff"
-
-
-    def color_theme_turquoise(self):
-        self.master.config(bg="#003333")
-        self.text_frame.config(bg="#003333")
-        self.text_box.config(bg="#669999", fg="#FFFFFF")
-        self.entry_frame.config(bg="#003333")
-        self.entry_field.config(bg="#669999", fg="#FFFFFF", insertbackground="#FFFFFF")
-        self.send_button_frame.config(bg="#003333")
-        self.send_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
-        #self.emoji_button.config(bg="#669999", fg="#FFFFFF", activebackground="#669999", activeforeground="#FFFFFF")
-        self.sent_label.config(bg="#003333", fg="#FFFFFF")
-
-        self.tl_bg = "#669999"
-        self.tl_bg2 = "#003333"
-        self.tl_fg = "#FFFFFF"    
-
-    # Blue
-    def color_theme_dark_blue(self):
-        self.master.config(bg="#263b54")
-        self.text_frame.config(bg="#263b54")
-        self.text_box.config(bg="#1c2e44", fg="#FFFFFF")
-        self.entry_frame.config(bg="#263b54")
-        self.entry_field.config(bg="#1c2e44", fg="#FFFFFF", insertbackground="#FFFFFF")
-        self.send_button_frame.config(bg="#263b54")
-        self.send_button.config(bg="#1c2e44", fg="#FFFFFF", activebackground="#1c2e44", activeforeground="#FFFFFF")
-        #self.emoji_button.config(bg="#1c2e44", fg="#FFFFFF", activebackground="#1c2e44", activeforeground="#FFFFFF")
-        self.sent_label.config(bg="#263b54", fg="#FFFFFF")
-
-        self.tl_bg = "#1c2e44"
-        self.tl_bg2 = "#263b54"
-        self.tl_fg = "#FFFFFF"
-
- 
-
-    # Hacker
-    def color_theme_hacker(self):
-        self.master.config(bg="#0F0F0F")
-        self.text_frame.config(bg="#0F0F0F")
-        self.entry_frame.config(bg="#0F0F0F")
-        self.text_box.config(bg="#0F0F0F", fg="#33FF33")
-        self.entry_field.config(bg="#0F0F0F", fg="#33FF33", insertbackground="#33FF33")
-        self.send_button_frame.config(bg="#0F0F0F")
-        self.send_button.config(bg="#0F0F0F", fg="#FFFFFF", activebackground="#0F0F0F", activeforeground="#FFFFFF")
-        #self.emoji_button.config(bg="#0F0F0F", fg="#FFFFFF", activebackground="#0F0F0F", activeforeground="#FFFFFF")
-        self.sent_label.config(bg="#0F0F0F", fg="#33FF33")
-
-        self.tl_bg = "#0F0F0F"
-        self.tl_bg2 = "#0F0F0F"
-        self.tl_fg = "#33FF33"
-
-    
-
-    # Default font and color theme
-    def default_format(self):
-        self.font_change_default()
-        self.color_theme_default()    
-
+    def color_theme_generic(self, colors):
+        self.master.config(bg=colors["bg2"])
+        self.text_frame.config(bg=colors["bg2"])
+        self.entry_frame.config(bg=colors["bg2"])
+        self.text_box.config(bg=colors["bg"], fg=colors["fg"])
+        self.entry_field.config(bg=colors["bg"], fg=colors["fg"], insertbackground=colors["fg"])
+        self.send_button_frame.config(bg=colors["bg2"])
+        self.send_button.config(bg=colors["bg"], fg=colors["fg"], activebackground=colors["bg"], activeforeground=colors["fg"])
+        self.sent_label.config(bg=colors["bg2"], fg=colors["fg"])
+        self.tl_bg = colors["bg"]
+        self.tl_bg2 = colors["bg2"]
+        self.tl_fg = colors["fg"]
+  
 
 if __name__ == "__main__":
     root=tk.Tk()
